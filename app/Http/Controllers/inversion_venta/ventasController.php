bb256ab4cdb130ca70e1d47744e33f3002e24e5a
Dayanna <montesgomezdayanna@gmail.com>
2024-04-28 14:57:43 -0500

    interfaz lista

--- app/Http/Controllers/inversion_venta/ventasController.php
+++ app/Http/Controllers/inversion_venta/ventasController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\inversion_venta;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\inversion_venta\ventas;
+use App\Models\inversion_venta\vehiculos;
 
 class ventasController extends Controller
 {
@@ -21,8 +22,9 @@ class ventasController extends Controller
     {
         try{
             $request->validate([
-                'cliente' => 'required|min:3|max:11',
-                'fk_ve_hiculo' => 'required|integer'
+                'cliente' => 'required|min:3|max:50',
+                'fk_ve_hiculo' => 'required|integer',
+                'precio_egreso' => 'required|integer'
             ]);
         } catch(\Illuminate\Validation\ValidationException $e){
             return response()->json(['errors' => $e->errors()], 422);
@@ -32,6 +34,10 @@ class ventasController extends Controller
         $ventas->cliente = $request->cliente;
         $ventas->fk_ve_hiculo = $request->fk_ve_hiculo;
         $ventas->save();
+
+        $vehiculo = vehiculos::find($request->fk_ve_hiculo);
+        $vehiculo->precio_egreso = $request->precio_egreso;
+        $vehiculo->save();
 
         return response()->json([
             'ok' => 'venta creada'
